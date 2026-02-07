2e4d00d0b78a4cbee7374ea89ce43f1587ba9625
Wittawat Buachan <wittawatbuachan@gmail.com>
2026-02-07 14:17:05 +0700

    update project: add Gemini LLM integration and tests

--- app/config.py
+++ app/config.py
@@ -1,14 +1,11 @@
 from pydantic_settings import BaseSettings
-from pydantic import ConfigDict
-
 
 class Settings(BaseSettings):
     mongo_uri: str
     mongo_db: str = "chatbot_db"
 
-    model_config = ConfigDict(
-        env_file=".env",
-        extra="ignore"
-    )
+    class Config:
+        env_prefix = ""   # สำคัญ
+        case_sensitive = True
 
 settings = Settings()
